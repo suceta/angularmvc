6b51743782610fe2ced245c6d4cd32a61f6f0396
Stalin Uceta <rafaeluceta09proj@gmail.com>
2025-03-11 17:50:28 -0400

    listado imagenes

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { DatePipe, NgFor, NgIf, NgOptimizedImage } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListatoPeliculaComponent } from "./peliculas/listato-pelicula/listato-pelicula.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListatoPeliculaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular-app';
+
+export class AppComponent  {
+
+
 }
